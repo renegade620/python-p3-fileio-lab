c5633af8e087257ceb1e70a3701f6af628a60ed7
Frankline Were <franklinegift@gmail.com>
2024-09-07 00:30:30 +0300

    write, append, read

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,13 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f"{file_name}.txt", mode="w", encoding="utf-8") as write_file:
+        write_file.write(file_content)
+
 
 def append_file(file_name, append_content):
-    pass
+    with open(f"{file_name}.txt", mode="a", encoding="utf-8") as append_file:
+        append_file.write(append_content)
+
 
 def read_file(file_name):
-    pass
+    with open(f"{file_name}.txt", mode="r", encoding="utf-8") as read_file:
+        return read_file.read()
